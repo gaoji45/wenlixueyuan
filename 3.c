e47dc9b0d68162414b762f18d858edf95efee26c
linux <358215993@qq.com>
2021-03-12 02:23:18 -0500

    add a printf

---
+++ 3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	float a,b,c;
+	scanf("%f,%f,%f",&a,&b,&c);
+	if(a+b>c&&a+c>b&&b+c>a)
+	{
+	    if(a==b &&a==c &&b==c)
+	{	printf("等边三角形");
+	}
+	    else if(a==b || a==c || b==c)
+	{	printf("等腰三角形");
+	}
+		else if(a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a)
+		{  printf("直角三角形");
+		}
+    else
+	printf("普通三角形");
+	}
+	else
+	printf("不是三角形");
+    return 0;
+}
